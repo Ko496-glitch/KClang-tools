c6d14b7dbeba40b5618d64ba837b3a5a33584925
kartikohlan <kartik7ohlan@gmail.com>
2026-03-08 18:29:48 -0400

    Added logic for heatmap

--- visitor.hpp
+++ visitor.hpp
@@ -6,16 +6,83 @@
 #include "clang/AST/Expr.h"
 
 
+using namespace clang;
+using namespace llvm;
+
+/*
+Traverse the AST, check if node == CallExpr
+1)if yes then check the callee, ask for the location from SourceLocation and update the llvm::denseMap
+2) move ahead
+*/
 
 namespace KClang{
+
+
+  class FunctionInfo{
+      
+    public:
+
+      llvm::StringRef name;
+      llvm::StringRef file;
+      uint16_t line;
+      uint16_t column;
+
+  };
 
 	class CallVisitor: public clang::RecursiveAASTVisitor<CallVisitor>{
 
 		private:
-			clang::ASTContext *Context;
-		public:
-			CallVisitor(clang::ASTContext *Context): Context(Context){} 
 
+			clang::ASTContext *Context;
+
+      llvm::DenseMap<const clang::FunctionDecl*, FunctionInfo>HMap;
+
+    public:
+
+			CallVisitor(clang::ASTContext *Context): Context(Context){} 
+      
+      bool VisitCallExpr(clang::CallExpr *S){
+        
+        if(const clang::FunctionDecl *FD = S->getDirectCallee()){
+
+          llvm::StringRef name = FD->getNameAsString();
+
+          llvm::StringRef FullQualifiedName = getQualifiedNameAsString();
+
+          clang::SourceLocation loc = FD->getLocation();
+
+          clang::SourceManager &Full_loc = Context->getSourceManager();
+
+          clang::PresumeLoc PLoc = FD->PresumeLoc(loc);
+
+          if(PLoc.isValid()){
+            
+            llvm::StringRef &file = PLoc.getFileName();
+            uint16_t line = PLoc.getLine();
+            uint16_t col = PLoc.getColumn();
+
+          }
+          
+          auto it = HMap.find(FD);
+          
+          if(it == HMap.end()){
+
+            FunctionInfo *info;
+            info->name = name;
+            info->file = file;
+            info->line = line;
+            info->col  = col;
+
+            HMap.insert(info);
+
+          }else{
+
+            ++HMap[FD];
+          }
+
+          return true;
+
+        }
 
 	};
 }
