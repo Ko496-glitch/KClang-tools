8ffca655d1e4dcb64254d9c73de40f4c52efd992
kartikohlan <kartik7ohlan@gmail.com>
2026-03-08 21:14:08 -0400

    Added Consumer.hpp

---
+++ consumer.hpp
@@ -0,0 +1,44 @@
+#include "clang/AST/AST.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/AST/decl.h"
+
+
+
+using namespace clang;
+using namespace llvm;
+
+
+/*
+
+  Consumer just store visitor and feed ASTContext to it.
+
+  ASTContext contains 1) All AST nodes
+                      2) Source location and Source Manager
+                      3) Memory allocator
+  this is enough for us to make it work :)
+
+
+ */
+
+namespace KClang{
+
+  class Consumer: public clang::ASTConsumer{
+
+    private:
+
+      CallVisitor Visitor;
+
+    public:
+
+      Consumer(clang::ASTContext *Context): Visitor(Context){}
+
+      void HandleTranslationUnit(clang::ASTContext *Context){
+          
+          Visitor.TraverseDecl(Context.getTranslatinUnitDecl);
+
+      }
+
+  };
+  
+  
+}
