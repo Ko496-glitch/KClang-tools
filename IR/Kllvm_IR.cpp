80b00e9e0429e38623c30cdedd2f7bff30c56ca1
kartikohlan <kartik7ohlan@gmail.com>
2026-03-26 13:14:54 -0400

    Added Basic Blocks and Ir builder

--- IR/Kllvm_IR.cpp
+++ IR/Kllvm_IR.cpp
@@ -24,7 +24,6 @@ std::unique_ptr<llvm::Module> MyModule(LLVMContext &Context){
         bar(var);
     }
 
-
     -------------------------IR Code for this would be-----------------------------------------
 
     define void @foo(i32 %arg, i32 %arg1){
@@ -54,9 +53,32 @@ std::unique_ptr<llvm::Module> MyModule(LLVMContext &Context){
    FunctionType *barTy   = llvm::FunctionType::get(VoidTy, ArrayRef<VoidTy>, false);
    Function     *barFunc = cast<Function>(MyModule->getOrInsertFunction("bar", barTy).getCallee());
 
-   //int foo(){}, since this is a definition we have to use different signature
+   //int foo(){}
    FunctionType *fooTy = llvm::Function::get(VoidTy, ArrayRef<VoidTy, Int32Ty>, false);
-   Function *fooFunc  = cast<Function>(MyModule->getOrInsertFunction("foo", fooTy).getCalleeO());
+   Function *fooFunc   = cast<Function>(MyModule->getOrInsertFunction("foo", fooTy).getCallee());
+   
+   
+
+
+   //After creating the function we move on to create the basic BasicBlock, for foo we would create 3 basic blocks
+   auto *BB1 = BasicBlock::create(Context, "bb1", fooFunc);
+   auto *BB2 = BasicBlock::create(Context, "bb2", fooFunc);
+   auto *BB3 = BasicBlock::create(Context, "bb3", fooFunc);
+
+  // Now we work on the instructions, first we invoke the IR Builder
+  
+  IRBuilder Builder(BB);
+
+  Value *I1 = Builder.CreateAlloc(Int32Ty); // a
+  Value *I2 = Builder.CreateAlloc(Int32Ty); // b
+  Value *I3 = Builder.CreateAlloc(Int32Ty); // var
+  
+  Value *Arg  = fooFunc->getArg(0);
+  Value *Arg1 = fooFunc->getArg(1);
+
+  Builder.CreateStore(Arg, I1);
+  Builder.CreateStore(Arg1, I2);
+  
 
 }
 
